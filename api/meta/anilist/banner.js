d47288a149042998aa5a9282ef63515c769fca93
SANDIPOP999 <106296204+SANDIPOP999@users.noreply.github.com>
2025-01-16 08:54:05 +0530

    Create banner.js

---
+++ api/meta/anilist/banner.js
@@ -0,0 +1,31 @@
+const { GraphQLClient } = require('graphql-request');
+const client = new GraphQLClient('https://graphql.anilist.co');
+
+const trendingBannerQuery = `
+  query {
+    Page(page: 1, perPage: 5) {
+      media(sort: TRENDING_DESC) {
+        id
+        title {
+          userPreferred
+        }
+        bannerImage
+      }
+    }
+  }
+`;
+
+export default async function handler(req, res) {
+  try {
+    const data = await client.request(trendingBannerQuery);
+    const banners = data.Page.media.map((anime) => ({
+      id: anime.id,
+      title: anime.title.userPreferred,
+      bannerImage: anime.bannerImage || 'No banner available',
+    }));
+
+    res.status(200).json({ trendingBanners: banners });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch trending anime banners' });
+  }
+}
